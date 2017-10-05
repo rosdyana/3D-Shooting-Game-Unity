7639e1721cc9098316a5b86f61fb638820bd0af0
rosdyana <rosdyana.kusuma@gmail.com>
2017-10-06 00:04:06 +0800

    + add menu +add loading bar

---
+++ Assets/Src/LoadScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour {
+
+    public GameObject loadingScreen;
+    public Slider slider;
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void loadScene(int idxLevel)
+    {
+        StartCoroutine(LoadAsynchornously(idxLevel));
+    }
+
+    IEnumerator LoadAsynchornously (int sceneIdx)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIdx);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+}
