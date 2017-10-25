0dc568dfafcc3ed2baee9580dc94734cf678af30
rosdyana <rosdyana.kusuma@gmail.com>
2017-10-25 15:12:10 +0800

    add tree and dancing rabbit

---
+++ Assets/Src/ObjectSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject bottleReference;
+    private Vector3 throwForce = new Vector3(0, 18, 0);
+    public Rigidbody rb;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnTarget", 0.5f, 6);
+    }
+
+    void SpawnTarget()
+    {
+        rb = GetComponent<Rigidbody>();
+        for (byte i = 0; i < 4; i++)
+        {
+            GameObject target = Instantiate(bottleReference, new Vector3(Random.Range(10, 30), Random.Range(-25, -35), -32), Quaternion.identity) as GameObject;
+            rb.AddForce(throwForce, ForceMode.Impulse);
+        }
+    }
+}
