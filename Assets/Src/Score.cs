855f91a089f89b17c28938691721be742aa5344d
rosdyana <rosdyana.kusuma@gmail.com>
2017-11-06 17:11:57 +0800

    add timer and score script WIP and make muzzle flash bigger

---
+++ Assets/Src/Score.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public PlayerShoot ps;
+    public Text scoreText;
+    [SerializeField]
+    private int point = 50;
+	
+	// Update is called once per frame
+	void Update () {
+        if (ps.hitme)
+            scoreText.text += point.ToString();
+	}
+}
---
+++ Assets/Src/Timer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public Text timer;
+    [SerializeField]
+    private float countdown = 60;
+	
+	// Update is called once per frame
+	void Update () {
+        countdown -= Time.deltaTime;
+        timer.text = countdown.ToString("0");
+	}
+}
